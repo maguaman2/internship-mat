b176ee43f32125d546c86177b7f09f2be35f9b13
Marco G <magbmail@gmail.com>
2024-11-29 08:59:47 -0500

    first commit

---
+++ src/main/kotlin/ec/edu/sudamericano/internship_mat/InternshipMatApplication.kt
@@ -0,0 +1,11 @@
+package ec.edu.sudamericano.internship_mat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InternshipMatApplication
+
+fun main(args: Array<String>) {
+	runApplication<InternshipMatApplication>(*args)
+}
---
+++ src/test/kotlin/ec/edu/sudamericano/internship_mat/InternshipMatApplicationTests.kt
@@ -0,0 +1,13 @@
+package ec.edu.sudamericano.internship_mat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InternshipMatApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
